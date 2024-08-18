9a287787cda806c45e8651fe912ee9be6b7c506a
Dhananjay Sarathe <dhananjay.s@quickads.ai>
2024-08-18 15:58:02 +0530

    BE all routers and functionalit completed P-1

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -59,4 +59,55 @@ router.post("/transfer", authMiddleWare, async (req, res) => {
 });
 */
 
+router.post("/transfer", authMiddleWare, async (req, res) => {
+  const session = await mongoose.startsSession();
+  session.startTransaction();
+  const { amount, to } = req.body;
+  const account = await User.findOne({ userId: req.userId }).session(session);
+
+  if (!account || account.balance < amount) {
+    await session.abortTransaction();
+    return res.status(400).json({
+      message: "Insuficient Balance",
+    });
+  }
+
+  const toAccount = await User.findOne({ userId: to }).session(session);
+  if (!toAccount) {
+    return res.status(400).json({
+      message: "Invalid Account",
+    });
+  }
+
+  // Deduction of amt
+  await Account.updateOne(
+    {
+      userId: req.userId,
+    },
+    {
+      $inc: {
+        balance: -amount,
+      },
+    }
+  ).session(session);
+
+  // Addition of amount
+  await Account.updateOne(
+    {
+      userId: to,
+    },
+    {
+      $inc: {
+        balance: amount,
+      },
+    }
+  ).session(session);
+
+  // commiting transaction
+  await session.commitTransaction();
+  res.json({
+    message: "Transfer successfull",
+  });
+});
+
 module.exports = router;
